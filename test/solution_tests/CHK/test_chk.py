1ac8b01eb27f2bcef85d6262e0210474f2df7936
sbx_user1051 <sbx_user1051@169.254.88.245>
2019-07-20 17:36:29 +0000

    Sat Jul 20 17:36:29 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -2,4 +2,4 @@ from solutions.CHK import checkout_solution
 
 class TestChk():
     def test_chk(self):
-        assert checkout_solution.checkout() == ""
+        assert checkout_solution.checkout(6) == ""
