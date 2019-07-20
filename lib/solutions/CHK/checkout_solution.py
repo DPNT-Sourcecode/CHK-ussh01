29c26c31bd2f62553ca0e4025368d9c64031f812
sbx_user1051 <sbx_user1051@169.254.88.245>
2019-07-20 17:42:29 +0000

    Sat Jul 20 17:42:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,4 +8,20 @@ def checkout(skus):
 	# Alphabets = SKUs
 	items = skus.split(' ')
 
-	
+	# Keep track of the total cost of the cart
+	cartTotal = 0
+	qtyA = 0
+	qtyB = 0
+	qtyC = 0
+	qtyD = 0
+	for item in items:
+		if item == 'A':
+			qtyA += 1
+		elif item == 'B':
+			qtyB += 1
+		elif item == 'C':
+			qtyC += 1
+		elif item == 'D':
+			qtyD += 1
+		else:
+			return -1
