7e263c8c94196bd5111fdc16b240845d2f973a56
sbx_user1051 <sbx_user1051@169.254.88.245>
2019-07-20 18:26:48 +0000

    Sat Jul 20 18:26:48 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,61 +3,49 @@ import string
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-	# CHECK FOR VALID INPUT
+	# Check if input is of type string
 	if type(skus) != str:
 		return -1
 
+	# Return zero if empty string
 	if len(skus) == 0:
 		return 0
 
-	# Keep track of the quantity of each item in the cart
+	# Initialize dictionary with all uppercase alphabet SKUs
 	itemQtys = {}
 	for letter in string.ascii_uppercase:
 		itemQtys[letter] = 0
-		
-	qtyA = 0
-	qtyB = 0
-	qtyC = 0
-	qtyD = 0
-	qtyE = 0
-	qtyF = 0
+
+	# Keep track of the quantity of each item in the cart
 	for item in skus:
-		if item == 'A':
-			qtyA += 1
-		elif item == 'B':
-			qtyB += 1
-		elif item == 'C':
-			qtyC += 1
-		elif item == 'D':
-			qtyD += 1
-		elif item == 'E':
-			qtyE += 1
-		elif item == 'F':
-			qtyF += 1
+		if item in itemQtys:
+			itemQtys[item] += 1
 		else:
-			return -1
+			return -1s
 
 	# Compute each individual SKUs totals
 
-	totalA = int((qtyA / 5)) * 200 # Favour Pack of 5
-	qtyA -= 5 * int(qtyA / 5)
-	totalA += ((qtyA % 3) * 50 + int((qtyA / 3)) * 130) # Computer for pack of 3 & remainder
+	# A
+	totalA = int((itemQtys['A'] / 5)) * 200 # Favour Pack of 5
+	itemQtys['A'] -= 5 * int(itemQtys['A'] / 5)
+	totalA += ((itemQtys['A'] % 3) * 50 + int((itemQtys['A'] / 3)) * 130) # Computer for pack of 3 & remainder
 
-	totalC = qtyC * 20
-	totalD = qtyD * 15
-	totalE = qtyE * 40
+	totalC = itemQtys['C'] * 20
+	totalD = itemQtys['D'] * 15
+	totalE = itemQtys['E'] * 40
 
 	# If E offer applies, update B quantity accordingly
 	totalB = 0
-	if qtyB != 0:
-		qtyB = qtyB - int(qtyE / 2)
-		totalB = ((qtyB % 2) * 30 + int((qtyB / 2)) * 45)
+	if itemQtys['B'] != 0:
+		itemQtys['B'] = itemQtys['B'] - int(itemQtys['E'] / 2)
+		totalB = ((itemQtys['B'] % 2) * 30 + int((itemQtys['B'] / 2)) * 45)
 
 
-	totalF = ((qtyF % 3) * 10 + int((qtyF / 3)) * 20)
+	totalF = ((itemQtys['F'] % 3) * 10 + int((itemQtys['F'] / 3)) * 20)
 
 	# Computer overall cart total
 	cartTotal = totalA + totalB + totalC + totalD + totalE + totalF
 
 	return cartTotal
+
 
